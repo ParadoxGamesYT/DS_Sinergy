914ba5d07086e87716f8a0d005803a6c38bf51d1
Neon <75301116+ParadoxGamesYT@users.noreply.github.com>
2023-10-09 10:16:25 +0300

    Add files via upload

---
+++ dz3/src/BinaryTree.java
@@ -0,0 +1,94 @@
+class BinaryTree {
+    Node root;
+
+    public BinaryTree() {
+        root = null;
+    }
+
+    public void insert(int value) {
+        root = insertRec(root, value);
+    }
+
+    private Node insertRec(Node root, int value) {
+        if (root == null) {
+            root = new Node(value);
+            return root;
+        }
+
+        if (value < root.value)
+            root.left = insertRec(root.left, value);
+        else if (value > root.value)
+            root.right = insertRec(root.right, value);
+
+        return root;
+    }
+
+    public void delete(int value) {
+        root = deleteRec(root, value);
+    }
+
+    private Node deleteRec(Node root, int value) {
+        if (root == null) {
+            System.out.println("ээээээ, где значение в дереве? А его нет!");
+            return root;
+        }
+
+        if (value < root.value)
+            root.left = deleteRec(root.left, value);
+        else if (value > root.value)
+            root.right = deleteRec(root.right, value);
+        else {
+            if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+
+            root.value = minValue(root.right);
+
+            root.right = deleteRec(root.right, root.value);
+        }
+
+        return root;
+    }
+
+    private int minValue(Node root) {
+        int minv = root.value;
+        while (root.left != null) {
+            minv = root.left.value;
+            root = root.left;
+        }
+        return minv;
+    }
+
+    public void inorderTraversal() {
+        inorderRec(root);
+    }
+
+    private void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.println(root.value);
+            inorderRec(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
+
+        System.out.println("просмотр бинарного дерева по порядку:");
+        tree.inorderTraversal();
+
+        System.out.println("удаление узла с 20:");
+        tree.delete(20);
+        System.out.println("просмотр после удаления:");
+        tree.inorderTraversal();
+    }
+}
